52b7d26a0b95e69ccf393786626b4e2958616579
sunil <sunilyadav770275@gmail.com>
2024-02-06 12:25:05 +0530

    third

--- src/main/java/com/example/SpringBootApp1/controller/WelcomeController.java
+++ src/main/java/com/example/SpringBootApp1/controller/WelcomeController.java
@@ -1,16 +1,21 @@
 package com.example.SpringBootApp1.controller;
 
-
 public class WelcomeController {
 
 	public String welcomeController() {
 		return "welcome to github";
 
 	}
-	
-	public int addition(int a,int b) {
+
+	public int addition(int a, int b) {
 		int c;
-		c=a+b;
+		c = a + b;
+		return c;
+	}
+
+	public int sub(int a, int b) {
+		int c;
+		c = a - b;
 		return c;
 	}
 }
